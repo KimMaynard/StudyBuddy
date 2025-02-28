26f9838ad16641b456058931d385fe1c1dafe748
JCSIStudent <113455662+LF92R@users.noreply.github.com>
2025-02-28 18:40:41 -0500

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,10 +20,6 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "StudyBuddy"
-<<<<<<< HEAD
-include(":app")
-include(":app")
-=======
 include(":frontend:app", ":backend") //Now with separated front-end, back-end, and AI modules
->>>>>>> frontAndBack-End
+
  
